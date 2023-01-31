6c0d834608c20db6ffd357bb5eae446fb302a1df
kws04181 <kimsungwon57@naver.com>
2023-01-31 13:33:21 +0900

    0131 예제들

--- 11_react/my-post/src/App.js
+++ 11_react/my-post/src/App.js
@@ -4,18 +4,18 @@ import PostDetail from './components/PostDetail';
 
 function App() {
   // 서버에서 데이터를 가져왔다고 가정
-  const [posts, setPosts] = useState(['리액트 잘 쓰러면?',
-    '자바스크립트 핵심 문법', '스타일링 가이드']);
+  const [posts, setPosts] = useState(['리액트 잘 쓰려면?', '자바스크립트 핵심 문법', '스타일링 가이드']);
 
   const [showPostDetail, setShowPostDetail] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+
   const [likeCount, setLikeCount] = useState([0, 0, 0]);
 
   return (
     <>
       {/* 상단 헤더 만들기 */}
-      <header className='header--dark'>
-        <h4>Kim Seong Won</h4>
+      <header className="header--dark">
+        <h4>Goni Post</h4>
         <nav>
           <ul>
             <li>트렌딩</li>
@@ -24,11 +24,28 @@ function App() {
         </nav>
       </header>
 
-      <div className='inner'>
-        {/* Quize : map()을 이용하여 포스트 반복 출력하기 */}
+      <div className="inner">
+        {/* 포스트 목록 */}
+        {/* <div className="list">
+          <h4>{posts[0]}</h4>
+          <p>2023년 1월 20일</p>
+          <p>by goni.kim</p>
+        </div>
+        <div className="list">
+          <h4>{posts[1]}</h4>
+          <p>2023년 1월 2일</p>
+          <p>by alice</p>
+        </div>
+        <div className="list">
+          <h4>{posts[2]}</h4>
+          <p>2022년 12월 20일</p>
+          <p>by herohero</p>
+        </div> */}
+      
+        {/* Quiz: map()을 이용하여 posts 반복 출력하기 */}
         {posts.map((post, index) => {
           return (
-            <div key={index} className='list'
+            <div key={index} className="list"
               onClick={() => {
                 setShowPostDetail(true);
                 setCurrentIndex(index);
@@ -36,26 +53,27 @@ function App() {
             >
               <h4>{post}</h4>
               <p>2023년 1월 20일</p>
-              <p>by SW.Kim</p>
+              <p>by goni.kim</p>
+
               <hr />
+
               <span onClick={() => {
                 const copyLikeCount = [...likeCount];
-                copyLikeCount[1]++;
-                setLikeCount([1]);
-              }}>
-                🤯 {likeCount[index]}
-              </span>
+                // TODO: 
+              }}
+              >💗 {likeCount[index]}</span>
+
             </div>
           );
         })}
-        {/* {showPostDetail ? <PostDetail posts={posts} /> : null} */}
-        {showPostDetail && <PostDetail posts={posts} setPosts={setPosts}
-          currentIndex={currentIndex} />}
-      </div>
 
+
+        {/* 포스트 상세보기 조건부 렌더링 */}
+        {/* {showPostDetail ? <PostDetail /> : null} */}
+        {showPostDetail && <PostDetail posts={posts} setPosts={setPosts} currentIndex={currentIndex} />}
+      </div>
     </>
   );
 }
-
 
 export default App;
